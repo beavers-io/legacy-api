60f25423f42982537cdf64b704d40eecfa11a194
Kyle Stevenson <kyle@kylestevenson.me>
2015-07-13 04:19:07 -0700

    reduce complexity of ScrapeSubjects Job

--- app/Jobs/Scraper/ScrapeSubjects.php
+++ app/Jobs/Scraper/ScrapeSubjects.php
@@ -1,129 +1,114 @@
 <?php namespace Courses\Jobs\Scraper;
 
 use Courses\Subject;
-
 use Illuminate\Database\Eloquent\Model;
 
-class ScrapeSubjects {
+class ScrapeSubjects
+{
 
-	const CATALOG_URL = 'http://catalog.oregonstate.edu/SOC.aspx';
-	const QUERY_PARAMS = '?level=all&amp;campus=%s&amp;term=%s';
+    const CATALOG_URL  = 'http://catalog.oregonstate.edu/SOC.aspx';
+    const QUERY_PARAMS = '?level=all&amp;campus=%s&amp;term=%s';
 
-	public function fire($job, $data)
-	{
-		$url = sprintf(self::CATALOG_URL);
-		$contents = file_get_contents($url);
+    public function fire($job, $data)
+    {
+        $url      = sprintf(self::CATALOG_URL);
+        $contents = file_get_contents($url);
 
-		$terms = $this->getTerms($contents);
-		$campuses = $this->getCampuses($contents);
+        $terms    = $this->getTerms($contents);
+        $campuses = $this->getCampuses($contents);
 
-		foreach ($campuses as $campus)
-		{
-			echo $campus;
-			// break;
-			array_map(function ($term) use ($campus)
-			{
-				\Queue::push('\Courses\Jobs\Scraper\ScrapeSubjects@scrape', [
-					'campus' => $campus,
-					'term' => $term
-				]);
-			}, $terms);
-			break;
-		}
+        $this->dispatchCampusJobs($terms, $campuses[0]);
 
-		// array_map(function ($campus) use ($terms)
-		// {
-		// }, $campuses);
-		
-		$job->delete();
-	}
+        $job->delete();
+    }
 
-	private function getMatches($contents, $delimiter, $pattern)
-	{
-		$subbed = substr($contents, strpos($contents, $delimiter));
-		$results = preg_match_all($pattern, $subbed, $matches);
+    private function dispatchCampusJobs($terms, $campus)
+    {
+        collect($terms)->each(function ($term) use ($campus) {
+            \Queue::push('\Courses\Jobs\Scraper\ScrapeSubjects@scrape', [
+                'campus' => $campus,
+                'term'   => $term,
+            ]);
+        });
+    }
 
-		$values = [];
-		for ($i = 0; $i < $results; $i++)
-		{
-			// if there is more than one group in the regex, return the
-			// group values as an array
+    private function getMatches($contents, $delimiter, $pattern)
+    {
+        $subbed  = substr($contents, strpos($contents, $delimiter));
+        $results = preg_match_all($pattern, $subbed, $matches);
 
-			if (sizeof($matches) > 2)
-			{
-				$details = [];
+        $values = [];
+        for ($i = 0; $i < $results; $i++) {
+            // if there is more than one group in the regex, return the
+            // group values as an array
 
-				for ($j = 1; $j < sizeof($matches); $j++)
-				{ 
-					$details []= trim($matches[$j][$i]);
-				}
+            if (sizeof($matches) > 2) {
+                $details = [];
 
-				$values []= $details;
-			}
-			else
-			{
-				$values []= trim($matches[1][$i]);
-			}
-		}
+                for ($j = 1; $j < sizeof($matches); $j++) {
+                    $details[] = trim($matches[$j][$i]);
+                }
 
-		return $values;
-	}
+                $values[] = $details;
+            } else {
+                $values[] = trim($matches[1][$i]);
+            }
+        }
 
-	private function getTerms($contents)
-	{
-		return $this->getMatches($contents,
-								 'ctl00_ContentPlaceHolder1_ddlTerm',
-								 '~<option[^>]+value="(\d+)">[^<]+\d+</option>~');
-	}
+        return $values;
+    }
 
-	private function getCampuses($contents)
-	{
-		return $this->getMatches($contents,
-								 'ctl00$ContentPlaceHolder1$ddlCampus',
-								 '~<option[^>]+value="([^"]+)">[^<]+</option>~');
-	}
+    private function getTerms($contents)
+    {
+        return $this->getMatches($contents,
+            'ctl00_ContentPlaceHolder1_ddlTerm',
+            '~<option[^>]+value="(\d+)">[^<]+\d+</option>~');
+    }
 
-	private function getSubjects($contents)
-	{
-		$subjects = $this->getMatches($contents,
-									  'ctl00_ContentPlaceHolder1_dlSubjects',
-									  '~<a href="[^"]+subjectcode=([A-Z]+)">([^\(]+)\s+\(~');
+    private function getCampuses($contents)
+    {
+        return $this->getMatches($contents,
+            'ctl00$ContentPlaceHolder1$ddlCampus',
+            '~<option[^>]+value="([^"]+)">[^<]+</option>~');
+    }
 
-		return array_map(function ($subj)
-		{
-			return [
-				'id' => $subj[0],
-				'name' => $subj[1],
-			];
-		}, $subjects);
-	}
+    private function getSubjects($contents)
+    {
+        $subjects = $this->getMatches($contents,
+            'ctl00_ContentPlaceHolder1_dlSubjects',
+            '~<a href="[^"]+subjectcode=([A-Z]+)">([^\(]+)\s+\(~');
 
-	public function scrape($job, $data)
-	{
-		$url = sprintf(self::CATALOG_URL . self::QUERY_PARAMS,
-					   $data['campus'] ?: 'corvallis', $data['term'] ?: '');
+        return array_map(function ($subj) {
+            return [
+                'id'   => $subj[0],
+                'name' => $subj[1],
+            ];
+        }, $subjects);
+    }
 
-		$contents = file_get_contents($url);
+    public function scrape($job, $data)
+    {
+        $url = sprintf(self::CATALOG_URL . self::QUERY_PARAMS,
+            $data['campus'] ?: 'corvallis', $data['term'] ?: '');
 
-		Model::unguard();
+        $contents = file_get_contents($url);
 
-		array_map(function ($subj)
-		{
-			try
-			{
-				if (is_null(Subject::find($subj)->first()))
-				{
-					$subj = Subject::create($subj);
-					print_r($subj->toArray());
-				}
+        Model::unguard();
 
-				\Queue::push('Courses\Jobs\Scraper\ScrapeSubject', [
-					'subject' => $subj['id']
-				]);
-			}
-			catch (Illuminate\Database\QueryException $e) { }
-		}, $this->getSubjects($contents));
+        array_map(function ($subj) {
+            try
+            {
+                if (is_null(Subject::find($subj)->first())) {
+                    $subj = Subject::create($subj);
+                    print_r($subj->toArray());
+                }
 
-		$job->delete();
-	}
+                \Queue::push('Courses\Jobs\Scraper\ScrapeSubject', [
+                    'subject' => $subj['id'],
+                ]);
+            } catch (Illuminate\Database\QueryException $e) {}
+        }, $this->getSubjects($contents));
+
+        $job->delete();
+    }
 }
